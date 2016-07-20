0ffe060e4c49023e0c5eff4b63fa3fcc805fde08
Nicola Hughes <nicolarhughes@gmail.com>
2016-07-20 20:54:40 +0100

    Button lights up a photon

--- imports/ui/Button.jsx
+++ imports/ui/Button.jsx
@@ -1,15 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom'
+import Particle from 'particle-api-js';
+import { Meteor } from 'meteor/meteor';
 
 export default class Click extends Component {
-  changeBackground(){
-    document.body.style.backgroundColor = this.props.click.color;
+
+  lightParticle(){
+    Meteor.call("lightParticle", "************", function(error, result) {
+      console.log(result);
+    });
   }
 
   render() {
     return (
       <li>
-        <button className={this.props.click.color}  onClick={this.changeBackground.bind(this)} >
+        <button className={this.props.click.color}  onClick={this.lightParticle} >
         {this.props.click.color.toUpperCase()}
         </button>
       </li>
--- server/main.js
+++ server/main.js
@@ -1,5 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import Particle from 'particle-api-js';
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.methods({
+  lightParticle: function(deviceId) {
+    var token;
+    var particle = new Particle();
+    particle.login({username: '*******@gmail.com', password: '******'}).then(
+      function(data){
+        token = data.body.access_token;
+        console.log('API call completed with token: ', token);
+        particle.callFunction({ deviceId: deviceId, name: 'digitalwrite', argument: 'D7:LOW', auth: token });
+      },
+      function(err) {
+        console.log('API call fail: ', err);
+      }
+    );
+  }
 });
